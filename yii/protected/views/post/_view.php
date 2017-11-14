887dcf9775e527f44a6f74385598b1be80539e64
DESKTOP-MM637RP\Tai Lui <tailm@solazu.com>
2017-11-14 16:27:18 +0700

    opdate

--- yii/protected/views/post/_view.php
+++ yii/protected/views/post/_view.php
@@ -1,20 +1,21 @@
-<div class="block-content-product">
-    <div class="block-image">
-        <?php echo CHtml::image(Helper::getPathUpload() . $data->thumnail, $data->title) ?>
-    </div>
-    <div class="block-content">
-	    <?php echo CHtml::link(CHtml::encode($data->title), $data->url, array('class' => 'title')); ?>
-        <div class="content">
-	        <?php
-	        $this->beginWidget('CMarkdown', array('purifyOutput'=>true));
-	        echo $data->content;
-	        $this->endWidget();
-	        ?>
+<div class="detail-product">
+    <div class="block-content-product">
+        <div class="block-image">
+            <?php echo CHtml::image(Helper::getPathUpload() . $data->thumnail, $data->title) ?>
+        </div>
+        <div class="block-content">
+            <?php echo CHtml::link(CHtml::encode($data->title), $data->url, array('class' => 'title')); ?>
+            <div class="content">
+                <?php
+                $this->beginWidget('CMarkdown', array('purifyOutput'=>true));
+                echo $data->content;
+                $this->endWidget();
+                ?>
+            </div>
+            <div class="nav">
+                <?php echo CHtml::link('Permalink', $data->url); ?> |
+                Last updated on <?php echo date('F j, Y',$data->update_time); ?>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="nav">
-	<?php echo CHtml::link('Permalink', $data->url); ?> |
-    Last updated on <?php echo date('F j, Y',$data->update_time); ?>
 </div>
